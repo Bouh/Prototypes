307bf51a08c70056f00c901cdbd27b62eb5aab33
Bouh <bouh.vivez@gmail.com>
2016-08-04 19:04:50 +0200

    start

--- serveur.js
+++ serveur.js
@@ -1,16 +1,28 @@
 var http = require('http');
 var fs = require('fs');
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+	
+    res.sendfile('dindex.html');
+});
+app.use(express.static(__dirname + '/export'));
+app.use(express.static(__dirname + '/public'));
 
 // Chargement du fichier index.html affiché au client
+/*
 var server = http.createServer(function(req, res) {
     fs.readFile('./index.html', 'utf-8', function(error, content) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(content);
     });
 });
+*/
 
 // Chargement de socket.io
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(app.listen((process.env.PORT || 80)));
 
 // Quand un client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket) {
@@ -26,5 +38,4 @@ io.sockets.on('connection', function (socket) {
     });	
 });
 
-
-server.listen(8080);
+//app.listen((process.env.PORT || 80));
