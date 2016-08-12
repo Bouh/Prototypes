d28cb2a7cf6cf1ca3e03a2381dd7a8c4d9093fe9
Bouh <bouh.vivez@gmail.com>
2016-08-12 22:44:41 +0200

    Validation pseudo

--- app/serveur.js
+++ app/serveur.js
@@ -13,47 +13,47 @@ app.get('/', function(req, res) {
 app.use(express.static(__dirname + '/export'));
 app.use(express.static(__dirname + '/public'));
 
-// Chargement du fichier index.html affiché au client
-/*
-var server = http.createServer(function(req, res) {
-	fs.readFile('./index.html', 'utf-8', function(error, content) {
-		res.writeHead(200, {"Content-Type": "text/html"});
-		res.end(content);
-	});
-});
-*/
-
 // Chargement de socket.io
 var io = require('socket.io').listen(app.listen((process.env.PORT || 80)));
 
 // Quand un client se connecte, on le note dans la console
-	io.sockets.on('connection', function (socket) {
-	console.log('Un client est connecté !');
-});
-
 io.sockets.on('connection', function (socket) {
+	console.log('Un client est connecté !');
+
 	socket.emit('message', 'Vous êtes bien connecté !');
-	socket.broadcast.emit('message', 'Un autre client vient de se connecter !');
+	//socket.broadcast.emit('message', 'Un autre client vient de se connecter !');
 	 
 	// Quand le serveur reçoit un signal de type "message" du client    
 	socket.on('message', function (message) {
-	console.log('Un client me parle ! Il me dit : ' + message);
+		console.log('Un client me parle ! Il me dit : ' + message);
 	});	
 	
 	socket.on('creation_joueur', function (message) {
 		console.log('Un client veux le pseudo suivant : ' + message);
-		
-		if(pseudo.indexOf(message) !== -1){
-			console.log("Le pseudo est pris.");
-			socket.emit('message_error', 'Le pseudo est déjà pris');
+
+		var patternRegex = /[\n\r\s]+/g;
+		var result = patternRegex.test(message);
+
+		//Le pseudo contient un espace ou un retour à la ligne
+		if(result === true){
+			socket.emit("message_serveur", 'Le pseudo contient un espace ou un retour à la ligne.');
+		//Le pseudo n'a pas d'espace ou de retour à la ligne
 		}else{
-			pseudo.push(message);
-			socket.emit('reception_liste_joueurs', pseudo.toString());
-			//console.log("Le pseudo est libre.");
-			console.log("Liste des pseudo push aux clients : " + pseudo.toString());
+			//Si le pseudo est déjà dans la tableau
+			if(pseudo.indexOf(message) !== -1){
+				console.log("Le pseudo est pris.");
+				socket.emit('message_serveur', 'Le pseudo est déjà pris');
+			//Sinon il est pas dans le tableau
+			}else{
+				socket.emit('message_serveur', 'Le pseudo est libre');
+			}
+			
 		}
-		
 	});
 	
-	
+	socket.on('validation_joueur', function (message) {
+		pseudo.push(message);
+		socket.emit('reception_liste_joueurs', pseudo.toString());
+		console.log('Liste des pseudo push aux clients : ' + pseudo.toString());
+	});
 });
